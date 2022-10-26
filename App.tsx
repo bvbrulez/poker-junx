0acb5afcaac90c530616073d7fd64f5577babc17
bvbrulez <31540373+bvbrulez@users.noreply.github.com>
2022-10-26 23:16:16 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Pokerjunx</h1>
+      <div>Tour 2023:</div>
+      <ul>
+        <li>Tel Aviv</li>
+        <li>Rom</li>
+        <li>Kroatien</li>
+      </ul>
+    </div>
+  );
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# poker-junx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-starter-typescript-r1bvhq)
